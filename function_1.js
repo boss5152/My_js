f519fa841ae6468945df3ee6d9c6b17be1addeab
boss5152 <skyapple0316@gmail.com>
2019-06-20 17:21:08 +0800

    20190620_lesson_complete

--- function_1.js
+++
@@ -1,8 +0,0 @@
-function checkTWId(id){
-    let ret = false;
-    let regex = /^[A-Z][12][0-9]{8}$/;
-    let result = id.match(regex);
-    if(id.length == 10){
-        let c1 = id.substp(0,1);
-    }
-}
